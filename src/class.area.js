2fbb469358695317c8569f80b1a0922513fcbd67
n-peugnet <n.peugnet@free.fr>
2018-03-08 18:45:24 +0100

    first polygon-shape commit

--- src/class.area.js
+++ src/class.area.js
@@ -170,6 +170,30 @@ class AreaPoly extends Area {
 	constructor(coords = [], href) {
 		super("poly", coords, href);
 	}
+
+	isValidShape() {
+		return this.coords.length >= 3;
+	}
+
+	isHover(x, y) {
+		var cornersX = this.coords.map(c => { return c.x });
+		var cornersY = this.coords.map(c => { return c.y });
+
+		var i, j = cornersX.length - 1;
+		var oddNodes = false;
+
+		var polyX = cornersX;
+		var polyY = cornersY;
+
+		for (i = 0; i < cornersX.length; i++) {
+			if ((polyY[i] < y && polyY[j] >= y || polyY[j] < y && polyY[i] >= y) && (polyX[i] <= x || polyX[j] <= x)) {
+				oddNodes ^= (polyX[i] + (y - polyY[i]) / (polyY[j] - polyY[i]) * (polyX[j] - polyX[i]) < x);
+			}
+			j = i;
+		}
+
+		return oddNodes;
+	}
 }
 
 class AreaDefault extends Area {
